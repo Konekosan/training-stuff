b6d94c12eb7b9f9b64b6439cbaa1f41557d20c30
Konekosan <benjamin.parageaud@viacesi.fr>
2026-04-01 14:41:20 +0200

    home page

--- src/app/home-component/home-component.component.ts
+++ src/app/home-component/home-component.component.ts
@@ -1,12 +1,24 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home-component',
   standalone: true,
-  imports: [],
+  imports: [RouterModule],
   templateUrl: './home-component.component.html',
   styleUrl: './home-component.component.css'
 })
-export class HomeComponentComponent {
+export class HomeComponentComponent implements OnInit{
+
+  ngOnInit(): void {
+  }
+
+  calculateDureeExp(): number{
+    const beginYear = 2021;
+    const currentYear = new Date().getFullYear();
+    return currentYear - beginYear;
+  }
+
 
 }
